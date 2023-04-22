406de3f5d958878f915f22bed500112a418680f6
Daniel <93354347+danziel1@users.noreply.github.com>
2023-04-22 19:59:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Get your token from developer.clashofclans.com Make sure that you keep "Bearer" before your token
 token = 'Bearer API_KEY'
@@ -7,33 +8,36 @@ webhook = 'WEBHOOK_URL'
 
 
 headers = {'Authorization': token}
-r = requests.get(f'https://api.clashofclans.com/v1/clans/%23{tag}/currentwar', headers=headers).json()
-needToAttack = []
 
-if r['state'] == 'inWar':
-    for num in range(len(r['clan']['members'])):
-        try:
-            if len(r['clan']['members'][num]['attacks']) != 2:
-                print(len(r['clan']['members'][num]['attacks']))
-                needToAttack.append(r['clan']['members'][num]['name']+":"+"1")
-        except:
-            needToAttack.append(r['clan']['members'][num]['name']+":"+"2")
+while True:
+    r = requests.get(f'https://api.clashofclans.com/v1/clans/%23{tag}/currentwar', headers=headers).json()
+    needToAttack = []
 
-message = ''
+    if r['state'] == 'inWar':
+        for num in range(len(r['clan']['members'])):
+            try:
+                if len(r['clan']['members'][num]['attacks']) != 2:
+                    print(len(r['clan']['members'][num]['attacks']))
+                    needToAttack.append(r['clan']['members'][num]['name']+":"+"1")
+            except:
+                needToAttack.append(r['clan']['members'][num]['name']+":"+"2")
 
-for i in needToAttack:
-    member, attacksLeft = i.split(':')
-    message += "> " + member + " has " + attacksLeft + " attacks left\n"
+    message = ''
 
-data = {
-    "username": "War Log",
-    # "content": "War Log",
-    "embeds": [ 
-    {
-        "description" : message,
-        "title" : ">> Unused Attacks <<"
+    for i in needToAttack:
+        member, attacksLeft = i.split(':')
+        message += "> " + member + " has " + attacksLeft + " attacks left\n"
+
+    data = {
+        "username": "War Log",
+        # "content": "War Log",
+        "embeds": [ 
+        {
+            "description" : message,
+            "title" : ">> Unused Attacks <<"
+        }
+        ]
     }
-    ]
-}
 
-r = requests.post(webhook, json = data)
+    requests.post(webhook, json = data)
+    time.sleep(3600)
